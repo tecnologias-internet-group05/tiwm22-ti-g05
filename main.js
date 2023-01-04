3e33733c49fc26caf61a117840977832fdb71f8c
joaoliveira6704 <joaopedrooliveira6704@gmail.com>
2023-01-04 01:09:34 +0000

    Footer., Background, and Button
    Added a footer.
    Created a seamless background.
    Added a button that makes the page go back to the top.
    
    Color still needs change.

---
+++ main.js
@@ -0,0 +1,9 @@
+const toTop = document.querySelector(".top-arrow");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
